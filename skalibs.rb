f02202f7b11434d81804fb1ab42b4e7dc80b76cd
Giovanni Merlino <gmerlino@unime.it>
2018-10-08 06:29:45 +0200

    fixes for audit strict compliance

--- skalibs.rb
+++ skalibs.rb
@@ -1,5 +1,5 @@
 class Skalibs < Formula
-  desc "skarnet.org general-purpose libraries"
+  desc "Skarnet.org general-purpose libraries"
   homepage "https://skarnet.org/software/skalibs/"
   url "https://www.skarnet.org/software/skalibs/skalibs-2.6.2.0.tar.gz"
   sha256 "12f9706b412367c45310ab971d204d959441895d3fea6e5e41ab8ef8d24b71b2"
@@ -14,5 +14,10 @@ class Skalibs < Formula
     system "./configure", "--disable-shared",
                           "--prefix=#{prefix}"
     system "make", "install"
+  end
+
+  # to be fixed
+  test do
+    system "#{bin}/ska", "--help"
   end
 end
